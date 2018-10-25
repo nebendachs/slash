89e205d83a9d8620df1aa18daa6cf5670e14ec92
Daniel Hipp <d.hipp@gmx.net>
2018-10-25 13:30:08 +0200

    added backarrow

--- app/src/main/java/de/sharknoon/slash/RegisterActivity.java
+++ app/src/main/java/de/sharknoon/slash/RegisterActivity.java
@@ -1,7 +1,10 @@
 package de.sharknoon.slash;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
     }
 }
