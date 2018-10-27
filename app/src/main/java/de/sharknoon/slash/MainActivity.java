665d53436f9fb5d2625d029ef01cd03a7a37ebe5
Patrick Kempf <pa.kempf@web.de>
2018-10-27 13:35:36 +0200

    Update of login and register screen

--- app/src/main/java/de/sharknoon/slash/MainActivity.java
+++ app/src/main/java/de/sharknoon/slash/MainActivity.java
@@ -2,7 +2,6 @@ package de.sharknoon.slash;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -16,23 +15,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.handleRegisterLink();
+        this.handleLoginButton();
+    }
+
+
+    private void handleRegisterLink() {
+
+        // Get text view element for registration and handover event listener
         TextView registerLink = findViewById(R.id.homeScreenRegisterLink);
         registerLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // Move on to registration page
                 Intent goToRegisterActivity = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(goToRegisterActivity);
             }
         });
+    }
 
+    private void handleLoginButton() {
+
+        // Get login button element and handover event listener
         Button loginButton = findViewById(R.id.homeScreenLoginButton);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                // Reset error message text viewer
                 TextView emailErrorTextView = findViewById(R.id.homeScreenWrongEmailTextView);
                 emailErrorTextView.setText("");
-
                 TextView passwordErrorTextView = findViewById(R.id.homeScreenWrongPasswordTextView);
                 passwordErrorTextView.setText("");
 
@@ -45,14 +58,13 @@ public class MainActivity extends AppCompatActivity {
                 String insertedPassword = passwordInput.getText().toString();
 
                 // Empty password
-                if(insertedEmail == null || insertedEmail.isEmpty()) {
+                if (insertedEmail == null || insertedEmail.isEmpty()) {
                     emailErrorTextView.setText(R.string.homeScreenEmptyEmailMessage);
                     return;
                 }
 
                 // Empty password
-                if (insertedPassword == null || insertedPassword.isEmpty()){
-
+                if (insertedPassword == null || insertedPassword.isEmpty()) {
                     passwordErrorTextView.setText(R.string.homeScreenIncorrectPasswordMessage);
                     return;
                 }
--- app/src/main/java/de/sharknoon/slash/RegisterActivity.java
+++ app/src/main/java/de/sharknoon/slash/RegisterActivity.java
@@ -1,10 +1,11 @@
 package de.sharknoon.slash;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,33 +16,75 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // Enable back arrow
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-     //   getSupportActionBar().setLogo(R.mipmap.ic_launcher_round);
-     //   getSupportActionBar().setDisplayUseLogoEnabled(true);
-
-        EditText usernameInput = findViewById(R.id.registerScreenUsernameInput);
-        EditText emailInput = findViewById(R.id.registerScreenEmailInput);
-        EditText passwordInput = findViewById(R.id.registerScreenPasswordInput);
-        EditText confirmPasswordInput = findViewById(R.id.registerScreenEmailInput);
-
-     //   TextView unsernameErrorTextView =
-
-        String insertedUsername = usernameInput.getText().toString();
-        String insertedEmail = emailInput.getText().toString();
-        String insertedPassword = passwordInput.getText().toString();
-        String insertedConfirmPassword = confirmPasswordInput.getText().toString();
-
-        if(insertedEmail == null || insertedEmail.isEmpty() || insertedUsername == null || insertedUsername.isEmpty()){
-
-
-
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        //   getSupportActionBar().setLogo(R.mipmap.ic_launcher_round);
+        //   getSupportActionBar().setDisplayUseLogoEnabled(true);
 
+        this.handleRegisterButton();
 
     }
 
-    public boolean onOptionsItemSelected(MenuItem item){
+    private void handleRegisterButton() {
+
+        // Get register button element
+        Button registerButton = findViewById(R.id.registerScreenRegisterButton);
+
+        // Set event listener
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Get input elements
+                EditText usernameInput = findViewById(R.id.registerScreenUsernameInput);
+                EditText emailInput = findViewById(R.id.registerScreenEmailInput);
+                EditText passwordInput = findViewById(R.id.registerScreenPasswordInput);
+                EditText confirmPasswordInput = findViewById(R.id.registerScreenConfirmPasswordInput);
+
+                // Reset error message text viewer
+                TextView usernameErrorTextView = findViewById(R.id.registerScreenUsernameErrorMessageTextView);
+                usernameErrorTextView.setText("");
+                TextView passwordErrorTextView = findViewById(R.id.registerScreenPasswordErrorMessageTextView);
+                passwordErrorTextView.setText("");
+
+                String insertedUsername = usernameInput.getText().toString();
+                String insertedEmail = emailInput.getText().toString();
+                String insertedPassword = passwordInput.getText().toString();
+                String insertedConfirmPassword = confirmPasswordInput.getText().toString();
+
+                // Check for empty username and e-mail
+                if (insertedEmail == null || insertedEmail.isEmpty() || insertedUsername == null || insertedUsername.isEmpty()) {
+                    usernameErrorTextView.setText(R.string.registerScreenEmptyUsernameMessage);
+                    return;
+                }
+
+                // Check for empty password
+                if(insertedPassword == null || insertedPassword.isEmpty() || insertedConfirmPassword == null || insertedConfirmPassword.isEmpty()){
+                    passwordErrorTextView.setText(R.string.registerScreenEmptyPasswordMessage);
+                    return;
+                }
+
+                // Check if passwords match
+                if(!insertedPassword.equals(insertedConfirmPassword)){
+                    passwordErrorTextView.setText(R.string.registerScreenNoMatchPasswordMessage);
+                    return;
+                }
+
+                // Check for password guidelines
+                if(!UserRegistration.checkForPasswordGuidelines(insertedPassword)){
+                    passwordErrorTextView.setText(R.string.registerScreenUnsafePasswordMessage);
+                    return;
+                }
+
+                new UserRegistration(insertedUsername, insertedEmail, insertedPassword);
+            }
+        });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
         finish();
         return true;
     }
