9042ac586881e89937092e61f1223af38942be8c
Daniel Hipp <d.hipp@gmx.net>
2018-10-31 17:26:35 +0100

    added 'USER_ALREADY_LOGGED_IN'

--- app/src/main/java/de/sharknoon/slash/Login/LoginResponseHandler.java
+++ app/src/main/java/de/sharknoon/slash/Login/LoginResponseHandler.java
@@ -2,12 +2,15 @@ package de.sharknoon.slash.Login;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 
+import de.sharknoon.slash.Activties.HomeScreeActivity;
 import de.sharknoon.slash.Activties.MainActivity;
+import de.sharknoon.slash.Activties.RegisterActivity;
 import de.sharknoon.slash.R;
 import de.sharknoon.slash.Registration.RegistrationResponse;
 
@@ -16,6 +19,7 @@ public class LoginResponseHandler {
     private static final String SERVER_RESPONSE_STATUS_OK = "OK";
     private static final String SERVER_RESPONSE_USER_DOES_NOT_EXIST = "USER_DOES_NOT_EXIST";
     private static final String SERVER_RESPONSE_WRONG_PASSWORD = "WRONG_PASSWORD";
+    private static final String SERVER_RESPONSE_USER_ALREADY_LOGGED_IN = "USER_ALREADY_LOGGED_IN";
 
     public static void handlerResponse(String serverResponse, Context context) {
         Gson gson = new Gson();
@@ -28,7 +32,14 @@ public class LoginResponseHandler {
 
             case SERVER_RESPONSE_STATUS_OK:
 
-                //ToDo: Move on
+                ((Activity) context).runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Activity loginActivity = (Activity) context;
+                        loginActivity.startActivity(new Intent(context, HomeScreeActivity.class));
+                    }
+                });
 
                 Log.d("Status", SERVER_RESPONSE_STATUS_OK);
                 break;
@@ -51,6 +62,17 @@ public class LoginResponseHandler {
                     public void run() {
                         MainActivity.disableLoadingScreen(true, context);
                         passwordErrorTextView.setText(R.string.homeScreenWrongPassword);                    }
+                });
+                break;
+            case SERVER_RESPONSE_USER_ALREADY_LOGGED_IN:
+                Log.d("Status", SERVER_RESPONSE_USER_ALREADY_LOGGED_IN);
+                ((Activity) context).runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        MainActivity.disableLoadingScreen(true, context);
+                        emailErrorTextView.setText(R.string.homeScreenUserAlreadyLoggedIn);
+                    }
                 });
                 break;
         }
