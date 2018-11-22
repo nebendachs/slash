15159ea102adbeddcfd5d91b2290c44d05444e1c
Daniel Hipp <d.hipp@gmx.net>
2018-11-22 13:48:10 +0100

    added pushy-library and functions to get a wroking push-message

--- app/src/main/java/de/sharknoon/slash/Activties/LoginActivity.java
+++ app/src/main/java/de/sharknoon/slash/Activties/LoginActivity.java
@@ -1,25 +1,51 @@
 package de.sharknoon.slash.Activties;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import java.util.concurrent.CompletableFuture;
 
 import de.sharknoon.slash.Login.UserLogin;
 import de.sharknoon.slash.R;
+import me.pushy.sdk.Pushy;
+import me.pushy.sdk.util.exceptions.PushyException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    SharedPreferences prefs;
+    final String key = "token";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Pushy.listen(this);
+
+        prefs = getSharedPreferences("de.sharknoon.slash", MODE_PRIVATE);
+
+        // Check whether the user has granted us the READ/WRITE_EXTERNAL_STORAGE permissions
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // Request both READ_EXTERNAL_STORAGE and WRITE_EXTERNAL_STORAGE so that the
+            // Pushy SDK will be able to persist the device token in the external storage
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+
         setContentView(R.layout.activity_login);
 
         // Open Websocket connection
@@ -47,13 +73,14 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void handleLoginStop(){
+    private void handleLoginStop() {
         //Get progression bar to stop Login
         ProgressBar bar = findViewById(R.id.progressBar);
         bar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               LoginActivity.disableLoadingScreen(true, v.getContext());            }
+                LoginActivity.disableLoadingScreen(true, v.getContext());
+            }
         });
 
     }
@@ -85,7 +112,7 @@ public class LoginActivity extends AppCompatActivity {
                 boolean passwordTrue = true;
 
                 //False EMail
-                if(insertedEmail.isEmpty()){
+                if (insertedEmail.isEmpty()) {
                     emailErrorTextView.setText(R.string.homeScreenIncorrectEmailMessage);
                     mailTrue = false;
                 }
@@ -97,23 +124,41 @@ public class LoginActivity extends AppCompatActivity {
                 }
 
                 // Try to login User
-                if(mailTrue && passwordTrue) {
+                if (mailTrue && passwordTrue) {
                     LoginActivity.disableLoadingScreen(false, v.getContext());
-                    new UserLogin(insertedEmail, insertedPassword);
+
+                    String deviceToken = CompletableFuture.supplyAsync(() -> {
+                        try {
+                            if(!prefs.contains(key)) {
+                                String token = Pushy.register(getApplicationContext());
+                                prefs.edit().putString(key,token).apply();
+                                return token;
+                            } else {
+                                return prefs.getString(key,"");
+                            }
+                        } catch (PushyException e) {
+                            Log.i("PushyException", String.valueOf(e));
+                            return "";
+                        }
+                    }).join();
+
+                    Log.i("Pushy", deviceToken);
+
+                    new UserLogin(insertedEmail, insertedPassword, deviceToken);
                 }
             }
         });
     }
 
-    public static void disableLoadingScreen(boolean b, Context context){
+    public static void disableLoadingScreen(boolean b, Context context) {
         RelativeLayout loadingScreen = ((Activity) context).findViewById(R.id.loadingScreen);
         EditText email = ((Activity) context).findViewById(R.id.homeScreenEmailInput);
         EditText password = ((Activity) context).findViewById(R.id.homeScreenPasswordInput);
         Button login = ((Activity) context).findViewById(R.id.homeScreenLoginButton);
         TextView register = ((Activity) context).findViewById(R.id.homeScreenRegisterLink);
-        TextView forgotPW =((Activity) context).findViewById(R.id.homeScreenForgotPasswordLink);
+        TextView forgotPW = ((Activity) context).findViewById(R.id.homeScreenForgotPasswordLink);
 
-        if(!b) {
+        if (!b) {
             loadingScreen.setVisibility(View.VISIBLE);
         } else {
             loadingScreen.setVisibility(View.GONE);
